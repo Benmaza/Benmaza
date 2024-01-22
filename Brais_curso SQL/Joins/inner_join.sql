7560c4b7118f4707eb27469253fabe84c328bdd4
Benmaza <ruben.maza@gmail.com>
2024-01-22 14:04:17 -0600

    se cambia a nomeclatura para comentarios

--- Brais_curso SQL/Joins/inner_join.sql
+++ Brais_curso SQL/Joins/inner_join.sql
@@ -1,10 +1,10 @@
-# usar el Union interna para unir dos tablas
+-- usar el Union interna para unir dos tablas
 SELECT *
 FROM users
 INNER JOIN dni;
 
-# uniendo dos tablas con el mismo nombre de columna
-# se puede usar el alias para diferenciarlas u y d
+-- uniendo dos tablas con el mismo nombre de columna
+-- se puede usar el alias para diferenciarlas u y d
 SELECT *
 FROM users u
 JOIN dni d
