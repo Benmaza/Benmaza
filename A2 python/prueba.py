604d6cb4e48635c2fdd9d5e81036c2423ad63316
Benmaza <ruben.maza@gmail.com>
2024-01-26 17:50:30 -0600

    actualizar statements

--- A2 python/prueba.py
+++ A2 python/prueba.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-df_train = pd.read_csv('train.csv')
-df_train.head(20)
+df_train = open("train.csv", "r")
+df_train = pd.read_csv(df_train)
+print(df_train.head(5))
+
+print(df_train.shape)
+
+
+
 
 
