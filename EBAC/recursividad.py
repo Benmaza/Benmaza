0a1f93d87cdd6b74c805bebdd9ab9e01603af9ed
Benmaza <ruben.maza@gmail.com>
2024-02-26 13:55:22 -0600

    mas statements

---
+++ EBAC/recursividad.py
@@ -0,0 +1,15 @@
+def imprimir(x):
+    if x>0:
+        imprimir(x-1)
+        print(x)
+   
+imprimir (3) 
+
+# suma de una lista sin recursividad
+def suma(lista):
+    suma-0
+    for i in range(0,len(lista)):
+        suma+=lista[i]
+        return suma
+    print(suma[6,3,4,2,10])
+    
