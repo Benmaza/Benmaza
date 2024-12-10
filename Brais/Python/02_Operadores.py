de9a3fdee55864b65afcf96ca29c40025f6cb1e2
benmaza <ruben.maza@gmail.com>
2024-12-10 14:58:36 -0600

    se inicia el curso de Brais y se avanza en el proyecto de EBAC

---
+++ Brais/Python/02_Operadores.py
@@ -0,0 +1,46 @@
+### Operadores Aritmeticos ###
+
+print("Suma: ", 3+4)
+print("Resta: ", 3-4)
+print("Multiplicación: ", 3*4)
+print("División: ", 3/3)
+print("Operacion de modulo: ", 10%2)
+print("Operacion de potencia: ", 3**3)
+print("Operacion de division entera: ", 10//3)
+
+
+print("\nhola " + "Python "+"Que tal")
+print("hola " + str(5))
+print("hola " *5)
+print ("hola " * (2**3))
+
+my_float = 2.5 * 2
+print(my_float, "es de clase",type(my_float))
+# convierto el float a entero
+print("hola ", + int(my_float))
+
+
+print("\n")
+
+### Operadores de comparación ###
+
+print(3>4)
+print(3<4)
+print(3>=4)
+print(3<=4)
+print(3==4)
+print(3!=4)
+print(3>4>2)
+print("hola" > "adios")
+print("hola" == "HOLA".lower())
+print("aaaa" < "aaaaa")
+print("aaaa" >= "abaa") # ordenacion lexicografica
+print(len("aaaa") == len("aaaaa")) # comparacion de longitud
+
+print("\n")
+
+### Operadores logicos ###
+
+print(3>4 and 3<4) # False
+print(3>4 or 3<4) # True
+print(not 3>4) # True
